f85faad03718a2d5a3bb69f54de561f741bde3d5
Enoch Lin <enoinoo@gmail.com>
2019-03-12 15:28:05 -0400

    updated asteroid colliders

---
+++ Assets/Scripts/AsteroidCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.GetComponent<Asteroid>())
+        {
+            Debug.Log("initial velocity is" + collider.GetComponent<Asteroid>().rb.velocity);
+            collider.GetComponent<Asteroid>().ChangeVelocity();
+            Debug.Log("new velocity is" + collider.GetComponent<Asteroid>().rb.velocity);
+        }
+    }
+}
